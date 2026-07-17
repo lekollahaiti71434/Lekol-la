e9a538edb4441c294913ca1e3662ed620f73b155
lekollahaiti71434 <lekollahaiti@gmail.com>
2026-07-16 21:26:28 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...............................",
+  apiKey: "AIzaSyB44LKYeYxkAfY8gFeSWiqc_if8smanS0k",
   authDomain: "lekol-la.firebaseapp.com",
   projectId: "lekol-la",
-  storageBucket: "lekol-la.appspot.com",
-  messagingSenderId: "..............",
-  appId: "1:..............:web:................"
+  storageBucket: "lekol-la.firebasestorage.app",
+  messagingSenderId: "460888704859",
+  appId: "1:460888704859:web:aaf5da7222d99c868afc7e"
 };
 
 const app = initializeApp(firebaseConfig);
